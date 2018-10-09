6c524391117461b9f0ca5e4d0278dce7e0eea838
agonzgarc <agonzgarc@gmail.com>
2018-10-09 19:25:58 +0200

    Fixed bug on config for evaluation for training images

--- research/object_detection/active_learner.py
+++ research/object_detection/active_learner.py
@@ -15,6 +15,7 @@ import functools
 import json
 import os
 import tensorflow as tf
+import imp
 
 from object_detection import trainer
 from object_detection import evaluator_al as evaluator
@@ -24,6 +25,18 @@ from object_detection.builders import model_builder
 from object_detection.utils import config_util
 from object_detection.save_subset_imagenetvid_tf_record import save_tf_record
 from object_detection.utils import label_map_util
+from object_detection.track_detections import track_detection
+from object_detection.utils import iou
+
+from PIL import Image
+from object_detection.utils import visualization_utils as vis_utils
+
+# Tracking module
+import siamfc.siamese as siam
+from siamfc.tracker import tracker
+from siamfc.parse_arguments import parse_arguments
+from siamfc.region_to_bbox import region_to_bbox
+
 
 tf.logging.set_verbosity(tf.logging.INFO)
 tf.logging.set_verbosity(tf.logging.INFO)
@@ -48,10 +61,11 @@ flags.DEFINE_string('perf_dir', '/home/abel/DATA/faster_rcnn/resnet101_coco/perf
 flags.DEFINE_string('data_dir', '/home/abel/DATA/ILSVRC/',
                     'Directory that contains data.')
 flags.DEFINE_string('pipeline_config_path',
-                    '/home/abel/DATA/faster_rcnn/resnet101_coco/configs/faster_rcnn_resnet101_imagenetvid-active_learning.config',
+                    #'/home/abel/DATA/faster_rcnn/resnet101_coco/configs/faster_rcnn_resnet101_imagenetvid-active_learning.config',
+                    '/home/abel/DATA/faster_rcnn/resnet101_coco/configs/faster_rcnn_resnet101_imagenetvid-active_learning_short.config',
                     'Path to a pipeline_pb2.TrainEvalPipelineConfig config '
                     'file. If provided, other configs are ignored')
-flags.DEFINE_string('name', 'Ent',
+flags.DEFINE_string('name', 'TCFPshort',
                     'Name of method to run')
 flags.DEFINE_string('cycles','10',
                     'Number of cycles')
@@ -73,7 +87,8 @@ FLAGS = flags.FLAGS
 data_info = {'data_dir': FLAGS.data_dir,
           'annotations_dir':'Annotations',
           'label_map_path': './data/imagenetvid_label_map.pbtxt',
-          'set': 'train_ALL_clean'}
+          #'set': 'train_ALL_clean'}
+          'set': 'train_ALL_clean_short'}
 
 # Harcoded keys to retrieve metrics
 keyBike = 'PascalBoxes_PerformanceByCategory/AP@0.5IOU/n03790512'
@@ -224,6 +239,174 @@ def select_entropy_video(dataset,videos,active_set,detections):
         return indices
 
 
+
+def convert_boxes_wh(box):
+    """ Detection boxes come as [ymin,xmin,ymax,xmax]
+        We need [x,y,w,h] for tracking
+    """
+    whbox = np.array([box[1],box[0],box[3]-box[1],box[2]-box[0]])
+    return whbox
+
+
+def normalize_box(box,w,h):
+    """ Input: [ymin, xmin,ymax,xmax]
+        Output: normalized by width and height of image
+    """
+    nbox = box.copy()
+    nbox[:,0] = nbox[:,0]/h
+    nbox[:,1] = nbox[:,1]/w
+    nbox[:,2] = nbox[:,2]/h
+    nbox[:,3] = nbox[:,3]/w
+    return nbox
+
+def track_detections(dataset,videos,active_set,detections,groundtruths):
+
+    thresh_detection = 0.5
+
+    # Tracker configuration
+    hp, evaluation, run, env, design = parse_arguments()
+
+    final_score_sz = hp.response_up * (design.score_sz - 1) + 1
+
+
+    # Reload module so element are in current graph, look for a better solution to this
+    tf.reset_default_graph()
+    imp.reload(siam)
+
+    #gtrack = tf.Graph()
+    #with gtrack.as_default() as gtr:
+        #with gtr.name_scope("gtrack"):
+    filename, image, templates_z, scores = siam.build_tracking_graph(final_score_sz, design, env)
+
+    # We have detections only for the labeled dataset, be careful with indexing
+    unlabeled_set = [i for i in range(len(dataset)) if i not in active_set]
+
+    # ARE DETECTIONS NMSed?
+    detected_boxes = detections['boxes']
+    detected_scores = detections['scores']
+    detected_labels = detections['labels']
+
+    gt_boxes = groundtruths['boxes']
+
+    for v in videos:
+
+        video_dir = os.path.join(FLAGS.data_dir,'Data','VID','train',v)
+
+        # Select frames in current video (even those with wrong GTs)
+        #frames = [f['filename'] for f in dataset if f['video'] == v]
+        frames = [[f['idx'],f['filename']] for f in dataset if f['video'] == v and f['verified']]
+
+        # Get maximium index of frames in video
+        max_frame = np.max([f[0] for f in frames])
+
+        # Get only those that are not labeled --> pick from these
+        frames_unlabeled = [f for f in frames if f[0] in unlabeled_set]
+
+        tc_sum = np.zeros(len(frames_unlabeled))
+        tc_contrib = np.zeros(len(frames_unlabeled))
+
+        for fu in frames_unlabeled:
+
+            idx_frame_video = [f[0] for f in frames].index(fu[0])
+
+            # Get boxes for current frame
+            boxes_frame = detected_boxes[unlabeled_set.index(fu[0])]
+            scores_frame = detected_scores[unlabeled_set.index(fu[0])]
+            labels_frame = detected_labels[unlabeled_set.index(fu[0])]
+
+            gt_frame = gt_boxes[unlabeled_set.index(fu[0])]
+
+            pdb.set_trace()
+
+            curr_im = Image.open(os.path.join(video_dir,frames[idx_frame_video][1]))
+            im_w,im_h = curr_im.size
+            vis_utils.draw_bounding_boxes_on_image(curr_im,normalize_box(gt_frame,im_w,im_h))
+            curr_im.show()
+
+            # Get surviving detections in frame
+            idx_good_det = scores_frame > thresh_detection
+
+            boxes_frame = boxes_frame[idx_good_det,:]
+            labels_frame = labels_frame[idx_good_det]
+
+            num_good_dets = labels_frame.shape[0]
+
+            for idx_det in range(num_good_dets):
+
+
+                # Visualize detection
+                #curr_im = Image.open(os.path.join(video_dir,frames[idx_frame_video][1]))
+
+                #vis_utils.visualize_boxes_and_labels_on_image_array(numpy.array(curr_im),boxes_frame[idx_det])
+
+                # Convert [x y x y] to [y x w h]
+                curr_box = convert_boxes_wh(boxes_frame[idx_det])
+
+                # Forward frames in the video
+                # I can't do this with list comprehension for some reason
+                #frame_list = [frames[i] for i in range(idx_frame_video+1,idx_frame_video+4) if frames[i] in frames]
+                frame_list = [os.path.join(video_dir,frames[idx_frame_video][1])]
+                for t in range(1,10):
+                    if idx_frame_video+t <= max_frame:
+                        frame_list.append(os.path.join(video_dir,frames[idx_frame_video+t][1]))
+
+                pos_x, pos_y, target_w, target_h = region_to_bbox(curr_box)
+                pdb.set_trace()
+                bboxes, speed = tracker(hp, run, design, frame_list, pos_x, pos_y, target_w, target_h, final_score_sz, filename, image, templates_z, scores, 1)
+
+
+            for t in range(1,4):
+                if idx_frame_video+t <= max_frame:
+                    iou = tracked_boxes[t]
+
+
+        # Two frame lists, one forward, one backward
+
+        # Only go through verified frames
+        frames_verified = [f['idx'] for f in dataset if f['video'] == v and f['verified']]
+
+        #for d in frames_verified
+            # Select three forward frames
+
+
+        ## Get only those that are not labeled
+        #frames = [f for f in frames if f in unlabeled_set]
+
+        ## If all frames of video are in active set, ignore video
+        #if len(frames) > 0:
+            ## Extract corresponding predictions
+            #det_frames = [predictions[unlabeled_set.index(f)] for f in frames]
+
+            ## Compute and summarize entropy
+            #ent = np.array(compute_entropy(det_frames))
+
+            ##idxR = random.randint(0,len(frames)-1)
+            #idxR = ent.argmax(0)
+            #indices.append(frames[idxR])
+        ##print("Selecting frame {} from video {} with idx {}".format(idxR,v,frames[idxR]))
+    #return indices
+
+
+
+def visualize_detections(dataset, unlabeled_set, detections,groundtruths):
+    detected_boxes = detections['boxes']
+    detected_scores = detections['scores']
+    detected_labels = detections['labels']
+
+    gt_boxes = groundtruths['boxes']
+
+    for f in dataset:
+        if f['idx'] in unlabeled_set:
+            im_path = os.path.join(FLAGS.data_dir,'Data','VID','train',f['video'],f['filename'])
+            curr_im = Image.open(im_path)
+            im_w,im_h = curr_im.size
+            gt_im = gt_boxes[unlabeled_set.index(f['idx'])]
+            vis_utils.draw_bounding_boxes_on_image(curr_im,normalize_box(gt_im,im_w,im_h))
+            det_im = detected_boxes[unlabeled_set.index(f['idx'])]
+            vis_utils.draw_bounding_boxes_on_image(curr_im,normalize_box(det_im[:2,],im_w,im_h),color='green')
+            curr_im.show()
+
+
 #def main(_):
 if __name__ == "__main__":
 
@@ -258,6 +441,8 @@ if __name__ == "__main__":
 
     # Save number of test frames, as config is modified with unlabeled set
     num_eval_frames = eval_config.num_examples
+    # Also original pointer to tfrecord
+    tfrecord_eval = eval_input_config.tf_record_input_reader.input_path[0]
 
     # Get info about full dataset
     dataset,videos = get_dataset(data_info)
@@ -319,7 +504,10 @@ if __name__ == "__main__":
             if ('Rnd' in name) or cycle==1:
                 indices = select_random_video(dataset,videos,active_set)
             else:
-                indices = select_entropy_video(dataset,videos,active_set,detected_boxes)
+                if ('Ent' in name):
+                    indices = select_entropy_video(dataset,videos,active_set,detected_boxes)
+                elif ('TCFP' in name):
+                    indices = track_detections(dataset,videos,active_set,detected_boxes,groundtruth_boxes)
 
             active_set.extend(indices)
 
@@ -409,12 +597,16 @@ if __name__ == "__main__":
                 unlabeled_set = [i for i in range(len(dataset)) if i not in active_set]
 
                 # Short version, only save a subset
-                #unlabeled_set = unlabeled_set[:100]
+                unlabeled_set = unlabeled_set[:100]
+
                 save_tf_record(data_info,unlabeled_set)
 
                 print('Unlabeled frames in the dataset: {}'.format(len(unlabeled_set)))
 
-                input_config.tf_record_input_reader.input_path[0] = data_info['output_path']
+                # Set number of eval images to number of unlabeled samples and point to tfrecord
+
+                eval_input_config.tf_record_input_reader.input_path[0] = data_info['output_path']
+                eval_config.num_examples = len(unlabeled_set)
 
                 eval_train_dir = train_dir + 'eval_train/'
 
@@ -423,7 +615,7 @@ if __name__ == "__main__":
                         dataset_builder.build(config)).get_next()
 
                 # Initialize input dict again (necessary?)
-                create_eval_train_input_dict_fn = functools.partial(get_next_eval_train, input_config)
+                create_eval_train_input_dict_fn = functools.partial(get_next_eval_train, eval_input_config)
 
                 graph_rewriter_fn = None
                 if 'graph_rewriter_config' in configs:
@@ -432,10 +624,6 @@ if __name__ == "__main__":
 
                 # Need to reset graph for evaluation
                 tf.reset_default_graph()
-
-                # Set number of eval images to number of unlabeled samples
-                eval_config.num_examples = len(unlabeled_set)
-
 
                 metrics, detected_boxes, groundtruth_boxes = evaluator.evaluate(
                   create_eval_train_input_dict_fn,
@@ -448,6 +636,7 @@ if __name__ == "__main__":
 
                 # Put boxes information somewhere
                 #pdb.set_trace()
+                #visualize_detections(dataset, unlabeled_set, detected_boxes, groundtruth_boxes)
 
                 print('Done computing detections in training set')
 
@@ -460,7 +649,10 @@ if __name__ == "__main__":
                return dataset_builder.make_initializable_iterator(
                    dataset_builder.build(config)).get_next()
 
+            # Restore eval configuration on test
             eval_config.num_examples = num_eval_frames
+            eval_input_config.tf_record_input_reader.input_path[0] = tfrecord_eval
+            eval_input_config.tf_record_input_reader.input_path[0] = data_info['output_path']
 
             # Initialize input dict again (necessary?)
             create_eval_input_dict_fn = functools.partial(get_next_eval, eval_input_config)
