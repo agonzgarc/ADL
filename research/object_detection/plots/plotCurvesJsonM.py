71691ae2e7899fbae07818324062601e259285fa
agonzgarc <agonzgarc@gmail.com>
2019-01-31 14:52:52 +0100

    Restored and improved plots

--- research/object_detection/plots/plotCurvesJsonM.py
+++ research/object_detection/plots/plotCurvesJsonM.py
@@ -3,101 +3,64 @@ import numpy as np
 import json
 import pdb
 
-data_dir = '/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/'
-save_name = 'FP_vs_FN'
+data_dir = '/home/abel/Documents/graphics/ADL/curves/'
+save_name = 'Rnd-Lst'
 
-#num_videos = 398
-#points = np.array(range(1,6))*num_videos
-
+budget = 3200
 
 json_files = [
-    #'/home/abel/DATA/faster_rcnn/resnet101_coco/performances/cluster/RndFromScratchr3c10.json',
-    #'/home/abel/DATA/faster_rcnn/resnet101_coco/performances/cluster/Rnd20KR1r1c10-total.json',
-    #'/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/RndClean-total.json',
-    #'/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/RndxVidFrom0-Total.json',
-    #'/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/TCFP_total.json',
-    '/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/FP_gtR1c20.json',
-    '/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/FN_gtR1c20.json',
-    '/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/RndR1c20.json',
-    '/datatmp/Experiments/Javad/tf/data/ILSVRC/performances/FPN_total.json']
+    '/home/abel/DATA/faster_rcnn/resnet50_coco/performances/Rnd-Total.json',
+    '/home/abel/DATA/faster_rcnn/resnet50_coco/performances/Lst-Total.json']
 
-#labels = ['Random','Random_with_freezing','TCFP','False Positive','False Negative']
-labels = ['False Positive','False Negative','Random','FPN']
-
-colors = ['b','c','r','g','m']
-linestyles = ['-','-','-','-']
+labels = ['Random', 'Least Confidence']
+colors = ['b',[1,.4,.7]]
+linestyles = ['-','-']
 
 num_curves = len(json_files)
 
-for i in range(num_curves):
 
+num_cycles = [6,6]
+num_runs = [5,5]
+
+
+first_cycle = 0
+
+points = np.array(range(np.max(num_cycles)+1))*budget
+
+for i in range(num_curves):
     with open(json_files[i]) as f:
         data = json.load(f)
 
-    if i==0:
-      rnd_avg = np.zeros(8)
-      rnd_std = np.zeros(8)
-      for cycle in range(1,9):	
-       print(i)
-       ap_cycle = np.array([data['R'+str(r)+'c'+str(cycle)][0] for r in range(1,2)])
-       rnd_avg[cycle-1] = ap_cycle.mean()
-       rnd_std[cycle-1] = ap_cycle.std()
-      points=range(1,9)
-      plt.errorbar(points,rnd_avg,rnd_std,color=colors[i],label=labels[i],linestyle=linestyles[i],marker='o',capsize=2)
+    rnd_avg = np.zeros(num_cycles[i]+1)
+    rnd_std = np.zeros(num_cycles[i]+1)
 
-    if i==1:
-      rnd_avg = np.zeros(10)
-      rnd_std = np.zeros(10)
-      for cycle in range(1,11):	
-       print(i)
-       ap_cycle = np.array([data['R'+str(r)+'c'+str(cycle)][0] for r in range(1,2)])
-       rnd_avg[cycle-1] = ap_cycle.mean()
-       rnd_std[cycle-1] = ap_cycle.std()
-      points=range(1,11)
-      plt.errorbar(points,rnd_avg,rnd_std,color=colors[i],label=labels[i],linestyle=linestyles[i],marker='o',capsize=2)
+    for cycle in range(first_cycle,num_cycles[i]+1):
+        ap_cycle = np.array([data['R'+str(r)+'c'+str(cycle)][0] for r in range(1,num_runs[i]+1)])
+        rnd_avg[cycle] = ap_cycle.mean()
+        rnd_std[cycle] = ap_cycle.std()
 
-    elif i==2:
-      rnd_avg = np.zeros(7)
-      rnd_std = np.zeros(7)
-      for cycle in range(1,8):	
-       print(i)
-       ap_cycle = np.array([data['R'+str(r)+'c'+str(cycle)][0] for r in range(1,2)])
-       rnd_avg[cycle-1] = ap_cycle.mean()
-       rnd_std[cycle-1] = ap_cycle.std()
-      points=range(1,8)
-      plt.errorbar(points,rnd_avg,rnd_std,color=colors[i],label=labels[i],linestyle=linestyles[i],marker='o',capsize=2)
+    plt.errorbar(points[first_cycle:num_cycles[i]+1],rnd_avg[first_cycle:],rnd_std[first_cycle:],color=colors[i],label=labels[i],linestyle=linestyles[i],marker='o',capsize=2)
 
-    elif i==3:
-      rnd_avg = np.zeros(8)
-      rnd_std = np.zeros(8)
-      for cycle in range(1,9):	
-       print(i)
-       ap_cycle = np.array([data['R'+str(r)+'c'+str(cycle)][0] for r in range(1,4)])
-       rnd_avg[cycle-1] = ap_cycle.mean()
-       rnd_std[cycle-1] = ap_cycle.std()
-      points=range(1,9)
-      plt.errorbar(points,rnd_avg,rnd_std,color=colors[i],label=labels[i],linestyle=linestyles[i],marker='o',capsize=2)
+#for i in range(num_curves):
+    #with open(json_files[i]) as f:
+        #data = json.load(f)
 
-    elif i==4:
-      rnd_avg = np.zeros(17)
-      rnd_std = np.zeros(17)
-      for cycle in range(1,18):	
-       print(i)
-       ap_cycle = np.array([data['R'+str(r)+'c'+str(cycle)][0] for r in range(1,4)])
-       rnd_avg[cycle-1] = ap_cycle.mean()
-       rnd_std[cycle-1] = ap_cycle.std()
-      points=range(1,18)
-      plt.errorbar(points,rnd_avg,rnd_std,color=colors[i],label=labels[i],linestyle=linestyles[i],marker='o',capsize=2)
+    #for r in range(1,num_runs[i]+1):
+        #ap_cycle = np.zeros(num_cycles[i]+1)
+        #for cycle in range(num_cycles[i]+1):
+            #print(cycle)
+            #ap_cycle[cycle] = data['R'+str(r)+'c'+str(cycle)][0]
+        #plt.plot(points[:num_cycles[i]+1],ap_cycle,label=labels[i]+str(r),linestyle=linestyles[i],marker='o')
+
+
 
 plt.legend()
 
-#plt.xticks(points,[str(f)+' f/v' for f in range(1,11)])
-plt.xticks(points,range(1,9))
-plt.xlabel('Number of frames/video')
+plt.xticks(points[first_cycle:],range(first_cycle,np.max(num_cycles)+1))
+plt.xlabel('Cycle')
+#plt.xlabel('Number of frames/video')
 plt.ylabel('mAP')
 
 plt.savefig(data_dir+save_name)
 
-#plt.show()
-
-
+plt.show()
